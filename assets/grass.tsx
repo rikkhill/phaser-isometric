0969777f9cd0333d25753fa9db03d7da8117d707
Rikk Hill <rikk@unashamedly.co.uk>
2018-10-23 00:55:43 +0100

    Integrating navmesh

---
+++ assets/grass.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass" tilewidth="64" tileheight="32" tilecount="672" columns="16">
+ <image source="grassland_tiles.png" width="1024" height="1344"/>
+ <terraintypes>
+  <terrain name="grassy" tile="0"/>
+  <terrain name="Watery" tile="304"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,0,0,0"/>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="304" terrain="1,1,1,1"/>
+ <tile id="305" terrain="1,1,1,1"/>
+ <tile id="306" terrain="1,1,1,1"/>
+ <tile id="307" terrain="1,1,1,1"/>
+ <tile id="308" terrain="1,1,1,1"/>
+ <tile id="309" terrain="1,1,1,1"/>
+ <tile id="310" terrain="1,1,1,1"/>
+ <tile id="311" terrain="1,1,1,1"/>
+ <tile id="312" terrain="1,1,1,1"/>
+ <tile id="313" terrain="1,1,1,1"/>
+ <tile id="314" terrain="1,1,1,1"/>
+ <tile id="315" terrain="1,1,1,1"/>
+ <tile id="316" terrain="1,1,1,1"/>
+ <tile id="317" terrain="1,1,1,1"/>
+ <tile id="318" terrain="1,1,1,1"/>
+ <tile id="319" terrain="1,1,1,1"/>
+</tileset>
